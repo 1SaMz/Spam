31c2042767d59422e175c2f4c51a3fac0d074218
1SaMz <44713603+1SaMz@users.noreply.github.com>
2018-11-03 01:51:36 +0300

    Update spam.js

--- spam.js
+++ spam.js
@@ -1,11 +1,11 @@
 ﻿const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('message', message => {
-    if (message.content === '5') {
+    if (message.content === 'سام') {
           let count = 0;
           let ecount = 0;
           for(let x = 0; x < 1000; x++) {
-            message.channel.send(`5`)
+            message.channel.send(`آنيكك خواتككم لعيوون ساام`)
               .then(m => {
                 count++;
               })

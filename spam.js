265fa0e6a977509609d3a006d71f10eac4b3ac22
1SaMz <44713603+1SaMz@users.noreply.github.com>
2018-11-03 01:30:01 +0300

    Add files via upload

---
+++ spam.js
@@ -0,0 +1,21 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('message', message => {
+    if (message.content === '5') {
+          let count = 0;
+          let ecount = 0;
+          for(let x = 0; x < 1000; x++) {
+            message.channel.send(`5`)
+              .then(m => {
+                count++;
+              })
+              
+            }
+          }
+    });
+    
+    
+
+	
+	
+client.login("NDczNzQzMzU0NjQ4MzMwMjYw.DqqrwQ.RnA59HkyCktl3CLNGThcTx-cI-w")
